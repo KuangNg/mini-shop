203a416e3228f3fe903ae7e2926444bb9e5c0cda
KuangNaiGen <15775023443@163.com>
2023-09-21 15:01:29 +0800

    feat Init Project

---
+++ src/stores/modules/member.ts
@@ -0,0 +1,42 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+// 定义 Store
+export const useMemberStore = defineStore(
+  'member',
+  () => {
+    // 会员信息
+    const profile = ref<any>()
+
+    // 保存会员信息，登录时使用
+    const setProfile = (val: any) => {
+      profile.value = val
+    }
+
+    // 清理会员信息，退出时使用
+    const clearProfile = () => {
+      profile.value = undefined
+    }
+
+    // 记得 return
+    return {
+      profile,
+      setProfile,
+      clearProfile,
+    }
+  },
+  // TODO: 持久化
+  {
+    persist: {
+      // 调整为兼容多端的API
+      storage: {
+        setItem(key, value) {
+          uni.setStorageSync(key, value)
+        },
+        getItem(key) {
+          return uni.getStorageSync(key)
+        },
+      },
+    },
+  },
+)
