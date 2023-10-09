0369e2796bec515280374dfe3514fd291dde8310
KuangNaiGen <15775023443@163.com>
2023-10-09 15:33:13 +0800

    feat: 解决合并冲突

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -36,7 +36,6 @@ onLoad(async () => {
 </script>
 
 <template>
-<<<<<<< HEAD
   <PageSkeleton v-if="!isFinish"></PageSkeleton>
   <view v-else class="viewport">
     <!-- 搜索框 -->
@@ -253,11 +252,4 @@ page {
     }
   }
 }
-=======
-  <slider :value="sliderVal" @changing="sliderChange" :min="0" :max="1" :step="0.1" />
-</template>
-
-<style lang="scss">
-//
->>>>>>> a02945c808a08280fdb6335fedc7603d0a61318b
 </style>
